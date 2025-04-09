99aae31136809247c782444692b9a437ce28c2ac
Mr Zhang <qq849352726@gmail.com>
2025-04-09 09:03:42 +0800

    change lang

--- CollectIp/CollectIp/settings_optimized.py
+++ CollectIp/CollectIp/settings_optimized.py
@@ -8,6 +8,11 @@ from pathlib import Path
 import os
 import sys
 
+
+# lang
+os.environ['LANG'] = 'zh_CN.UTF-8'
+os.environ['LC_ALL'] = 'zh_CN.UTF-8'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
