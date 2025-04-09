c9bf2a7bca61e4048e149178c3c958839a7bcc64
Mr Zhang <qq849352726@gmail.com>
2025-04-09 14:37:12 +0800

    modify Celery settings

--- CollectIp/__init__.py
+++ CollectIp/__init__.py
@@ -7,12 +7,3 @@
 @Time : 2025/3/23 21:43
 
 """
-from __future__ import absolute_import, unicode_literals
-
-# 这将确保在Django启动时加载Celery app
-from .CollectIp.celery import app as celery_app
-
-__all__ = ('celery_app',)
-
-import pymysql
-pymysql.install_as_MySQLdb()
