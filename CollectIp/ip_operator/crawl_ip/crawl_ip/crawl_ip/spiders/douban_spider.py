5c1a62ca5093279e85ccc2ed52578d11b98c51f8
Mr Zhang <qq849352726@gmail.com>
2025-04-10 09:07:16 +0800

    fix bug： a unique value for --user-data-dir argument

--- CollectIp/ip_operator/crawl_ip/crawl_ip/crawl_ip/spiders/douban_spider.py
+++ CollectIp/ip_operator/crawl_ip/crawl_ip/crawl_ip/spiders/douban_spider.py
@@ -350,7 +350,8 @@ class DoubanSpider(scrapy.Spider):
                 options.add_argument('--window-size=1920,1080')
                 options.add_argument('--no-sandbox')
                 options.add_argument('--disable-dev-shm-usage')
-                
+                options.add_argument('--chrome-skip-compat-layer-relaunch')
+
                 # 清理目前可能存在的旧浏览器进程
                 try:
                     import psutil
