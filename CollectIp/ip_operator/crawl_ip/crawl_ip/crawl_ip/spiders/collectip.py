826c3d4d1976af83b56017f2aac239947677c0d4
Mr Zhang <qq849352726@gmail.com>
2025-04-14 19:13:51 +0800

    fix collectip spider path

--- CollectIp/ip_operator/crawl_ip/crawl_ip/crawl_ip/spiders/collectip.py
+++ CollectIp/ip_operator/crawl_ip/crawl_ip/crawl_ip/spiders/collectip.py
@@ -175,12 +175,7 @@ class CollectipSpider(scrapy.Spider):
 
         try:
             # 自动下载和配置 ChromeDriver
-            try:
-                driver_path = ChromeDriverManager().install()
-            except ValueError as e:
-                logger.warning(f"ChromeDriver自动安装失败: {e}，使用备用路径")
-                driver_path = r'D:\chorme_download\chromedriver-win32\chromedriver.exe'
-
+            driver_path = r'/usr/local/CollectIp/CollectIp/ip_operator/chromedriver-linux64/chromedriver'
             # 创建 Service 对象，传入 ChromeDriver 的路径
             service = Service(driver_path)
 
